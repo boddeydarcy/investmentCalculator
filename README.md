8cf1cbf5258b31f9c59a06519237da568709023a
Darcy Boddey <boddeydarcy@gmail.com>
2024-11-22 14:55:08 +0000

    added checking if investment is net pos or neg

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # investmentCalculator
+Program to track my investments, this will show percentage gain and loss, how much money is invested, and will forecast over the next period what it wil be worth.
--- main.py
+++ main.py
@@ -14,8 +14,10 @@ class Investment:
 		return tempInvestments
 
 	def removeInvestments(self, name, amount, date):
-		investment = [name, amount, date]
-		self.investments.append(investment)
+		n = len(self.investments)
+		for i in range(n):
+			if self.investments[i][0] == name and self.investments[i][1] == amount and self.investments[i][2] == date:
+				self.investments.remove(self.investments[i])
 
 	def totalInvestedPer(self, indexName):
 		investment = 0
@@ -23,7 +25,24 @@ class Investment:
 		for i in range(n):
 			if self.investments[i][0] == indexName:
 				investment += self.investments[i][1]
+		return investment
+
+	def printInvestment(self, indexName):
+		investment = self.totalInvestedPer(Investment, indexName)
 		print("Total invested into", indexName, "is", investment)
+
+	def checkInvestmentPer(self, indexName, currentPrice):
+		investment = self.totalInvestedPer(Investment, indexName)
+		diff = currentPrice - investment
+		if diff > 0:
+			percentageDiff = diff / investment * 100
+			print("Investment Is Positive, Has increased by", percentageDiff, "%")
+		elif diff == 0:
+			print("Investment hasnt changed")
+		else:
+			percentageDiff = (investment - currentPrice) / investment * 100
+			print("Investment Is Negative, Has decreased by", percentageDiff, "%")
+
 
 	def totalInvested(self):
 		investment = 0
@@ -43,4 +62,5 @@ Investment.removeInvestments(Investment,"S&P500", -50, "22/11/24")
 print(Investment.returnInvestments(Investment))
 print(Investment.getIndexInvesments(Investment, "S&P500"))
 Investment.totalInvested(Investment)
-Investment.totalInvestedPer(Investment, "S&P500")
+Investment.printInvestment(Investment, "S&P500")
+Investment.checkInvestmentPer(Investment, "S&P500", 50)
